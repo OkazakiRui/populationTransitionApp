8f5c9252d9a064888a87f7fe18b76b59fe82d2ab
RuiOkazaki <okazaki200226@gmail.com>
2022-03-05 18:56:16 +0900

    feat: グラフに描画するデータを保存する場所を作成した

---
+++ src/recoil/seriesData.ts
@@ -0,0 +1,48 @@
+import { atomFamily, selectorFamily } from 'recoil';
+
+import apiClient from 'library/apiClient';
+import { PopulationResult } from 'types/population';
+import { SeriesData } from 'types/series';
+
+// グラフに描画するデータのキャッシュを保存します
+const seriesCacheAtom = atomFamily<SeriesData | null, number>({
+  key: 'seriesAtom',
+  default: null,
+});
+
+const seriesSelector = selectorFamily<
+  SeriesData,
+  { prefName: string; prefCode: number }
+>({
+  key: 'seriesSelector',
+  get:
+    ({ prefName, prefCode }) =>
+    async ({ get }) => {
+      // キャッシュにデータがある場合はキャッシュを返します
+      const currentValue = get(seriesCacheAtom(prefCode));
+      if (currentValue) return currentValue;
+
+      const { data } = await apiClient.get<PopulationResult>(
+        '/population/composition/perYear',
+        {
+          params: { prefCode },
+        },
+      );
+
+      // 各年代の総人口を配列にします
+      const totalPopulation = // eslint-disable-line operator-linebreak
+        data.message === null
+          ? data.result.data[0].data.map((dt) => dt.value)
+          : [0];
+
+      // グラフに描画するデータを作って返します。
+      const series: SeriesData = {
+        type: 'line',
+        data: totalPopulation,
+        name: prefName,
+      };
+
+      return series;
+    },
+});
+export default seriesSelector;
